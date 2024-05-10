ff587040f0c942c7f626c4e59886001767ade375
Jaehoon Kim <46511592+redman1234321@users.noreply.github.com>
2024-05-10 23:47:13 +0900

    fork

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 struct stat stat1, stat2;
 struct tm *time1, *time2;
 
-void filestat1();
-void filestat2();
+void filestat1(char *filename);
+void filestat2(char *filename);
 void filetime1();
 void filetime2();
 void sizecmp();
@@ -17,52 +17,54 @@ void datecmp();
 void timecmp();
 
 int main(){
-    filestat1();
-    filestat2();
+    char filename1[] = "file1.txt"; // 첫 번째 파일 이름
+    char filename2[] = "file2.txt"; // 두 번째 파일 이름
+
+    filestat1(filename1);
+    filestat2(filename2);
     filetime1();
     filetime2();
     sizecmp();
     blockcmp();
     datecmp();
     timecmp();
+
+    return 0;
 }
 
-//파일 1의 정보를 가져오는 함수 작성
-void filestat1(){
+void filestat1(char *filename){
     
 }
 
-//파일 2의 정보를 가져오는 함수 작성
-void filestat2(){
-    
+void filestat2(char *filename){
 }
 
-//파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+   
 }
 
-//파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
+   
 }
 
-//두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
-    
+    if (stat1.st_size == stat2.st_size)
+        printf("파일 크기가 같습니다.\n");
+    else
+        printf("파일 크기가 다릅니다.\n");
 }
 
-//두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(){
-    
+    if (stat1.st_blocks == stat2.st_blocks)
+        printf("파일 블록 수가 같습니다.\n");
+    else
+        printf("파일 블록 수가 다릅니다.\n");
 }
 
-//두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
     
 }
 
-//두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+   
 }
